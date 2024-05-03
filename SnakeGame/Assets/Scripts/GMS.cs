66864aa847c463ed012848f033e963d28f73da53
4-Axolotl <angel_alejandro_aleman@hotmail.com>
2024-05-03 13:29:17 -0600

    Bug Fixes and add build file

--- SnakeGame/Assets/Scripts/GMS.cs
+++ SnakeGame/Assets/Scripts/GMS.cs
@@ -9,7 +9,7 @@ public class GMS : MonoBehaviour
 
     private void Start()
     {
-        for (int i = -5; i <= 4; i++)
+        for (int i = -7; i <= 6; i++)
         {
             for (int j = -14; j <= 13; j++)
             {
@@ -18,16 +18,16 @@ public class GMS : MonoBehaviour
                 Ground.transform.position = new Vector3((float)(j + 0.5), (float)(i + 0.5), 1);
             }
         }
-        for (int j = -14; j <= 13; j++)
+        for (int j = -16; j <= 15; j++)
         {
             GameObject Walld = new GameObject("DarkGrass", typeof(SpriteRenderer));
             GameObject Wallu = new GameObject("DarkGrass", typeof(SpriteRenderer));
             Walld.GetComponent<SpriteRenderer>().sprite = darkgrass;
             Wallu.GetComponent<SpriteRenderer>().sprite = darkgrass;
-            Walld.transform.position = new Vector3((float)(j + 0.5), (float)(-6 + 0.5), 1);
-            Wallu.transform.position = new Vector3((float)(j + 0.5), (float)(5 + 0.5), 1);
+            Walld.transform.position = new Vector3((float)(j + 0.5), (float)(-8 + 0.5), 0);
+            Wallu.transform.position = new Vector3((float)(j + 0.5), (float)(7 + 0.5), 0);
         }
-        for (int i = -6; i <= 5; i++)
+        for (int i = -7; i <= 6; i++)
         {
             for (int j = 0; j < 2; j++)
             {
@@ -35,8 +35,8 @@ public class GMS : MonoBehaviour
                 GameObject Wallr = new GameObject("DarkGrass", typeof(SpriteRenderer));
                 Walll.GetComponent<SpriteRenderer>().sprite = darkgrass;
                 Wallr.GetComponent<SpriteRenderer>().sprite = darkgrass;
-                Walll.transform.position = new Vector3((float)(-15 - j + 0.5), (float)(i + 0.5), 1);
-                Wallr.transform.position = new Vector3((float)(14 + j + 0.5), (float)(i + 0.5), 1);
+                Walll.transform.position = new Vector3((float)(-15 - j + 0.5), (float)(i + 0.5), 0);
+                Wallr.transform.position = new Vector3((float)(14 + j + 0.5), (float)(i + 0.5), 0);
             }
         }
     }
